4b5c7c73fc9676d38d78b537680d2766d03fe14f
RMRBoot <rhettmrichardson@gmail.com>
2021-11-06 19:09:22 -0600

    reverting broken code

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -15,22 +15,30 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // function to generate password
-var generatePassword = function() {
+const generatePassword = function() {
 
   const numChars = prompt ("Length of password?"); {
 
-  if (!isNaN(numChars) && numChars >= 8 && numChars <= 128 {
+  // if (!isNaN(numChars) && numChars >= 8 && numChars <= 128) {
+    // const lowCase = confirm("Use lowercase letters?");
+    // if (lowCase === true) {
+      // textArr.push("low");
+    }
 
   
   
   
-    var textArr = [];
+    // var textArr = [];
+    // console.log (textArr)
   
     // let passwordText = ''; 
 
   // for (i = 1; i <= numChars; i++) {
 
 
-  }
+
 
 }
+
+
+// const low = ()
